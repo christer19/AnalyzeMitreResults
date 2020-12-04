eb17557d7364837c26a951160d7e511317a4d320
jcarterbriggs <jcarterbriggs@gmail.com>
2020-12-04 12:40:59 -0500

    Initial submission

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import InitializeMitre
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +11,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    InitializeMitre.downloadFiles()
+    InitializeMitre.createDictionary()
+    df = InitializeMitre.createDataFrame()
+    print(df.head())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
